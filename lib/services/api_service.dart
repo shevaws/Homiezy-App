19fc010c09f3d7466a995ea83fa14ebd6a41e225
shevaws <xhuagf299@gmail.com>
2026-06-14 21:36:11 +0700

    Alhamdulillah kelar

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://10.0.2.2:8000/api/v1';
+  static const String baseUrl = 'https://homiezy.up.railway.app/api/v1';
   // 10.0.2.2 = localhost dari emulator Android
   // Kalau pakai device fisik, ganti dengan IP laptop kamu (cek ipconfig)
 
